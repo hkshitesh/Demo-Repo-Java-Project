f5c99a54e3e1f9bc922307c00f1ce9ee97385176
Hitesh Kumar Sharma <hiteshupes@gmail.com>
2022-12-25 00:20:24 +0530

    first commit

---
+++ src/pkg1/Calculator.java
@@ -0,0 +1,19 @@
+package pkg1;
+
+public class Calculator {
+	public void sum(int a, int b)
+	{
+		System.out.println(a+b);
+	}
+	public void diff(int a, int b)
+	{
+		System.out.println(a-b);
+	}
+
+	public static void main(String[] args) {
+		
+		Calculator ob = new Calculator();
+		ob.sum(10, 5);
+		ob.diff(10, 5);
+	}
+}

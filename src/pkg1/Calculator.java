8caf5f84da26c3196c97a1f3ed9b7abe346578f9
unknown <Administrator@HITESHKUMAR_LTP.DDN.UPES.AC.IN>
2022-12-25 22:52:13 +0530

    fifith commit

--- src/pkg1/Calculator.java
+++ src/pkg1/Calculator.java
@@ -9,6 +9,10 @@ public class Calculator {
 	{
 		System.out.println(a-b);
 	}
+	public void mul(int a, int b)
+	{
+		System.out.println(a*b);
+	}
 	public static void main(String[] args) {
 		
 		Calculator ob = new Calculator();

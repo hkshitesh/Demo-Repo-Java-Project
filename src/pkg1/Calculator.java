1f644fd19013dc6ad8f949650239871882256b3c
Hitesh Kumar Sharma <hiteshupes@gmail.com>
2022-12-25 22:56:02 +0530

    sixth commit

--- src/pkg1/Calculator.java
+++ src/pkg1/Calculator.java
@@ -13,6 +13,10 @@ public class Calculator {
 	{
 		System.out.println(a*b);
 	}
+	public void div(int a, int b)
+	{
+		System.out.println(a/b);
+	}
 	public static void main(String[] args) {
 		
 		Calculator ob = new Calculator();
